41a3b0adf377b09ec0f81bc80925653e2d9496c3
aidenjl193 <aidenjl193@gmail.com>
2019-05-22 12:31:13 +0100

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,10 +8,12 @@ class PostsController < ApplicationController
   end
 
   def update
-    @post.update(post_params)
+    # @post.update(post_params)
+    @post.assign_attributes(post_params)
 
 
     if(@post.valid?)
+      @post.save
       redirect_to post_path(@post)
     else
       render :edit
